be14cb9d5055d582225e79866062606a626034ce
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-05-28 20:50:23 -0300

    Added Errors

--- src/main/java/ar/com/saile/demojwt/controllers/ErrorHandlerController.java
+++ src/main/java/ar/com/saile/demojwt/controllers/ErrorHandlerController.java
@@ -6,11 +6,27 @@ import ar.com.saile.demojwt.exceptions.MissingHeaderInfoException;
 import ar.com.saile.demojwt.exceptions.RecordNotFoundException;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
@@ -24,6 +40,177 @@ import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class ErrorHandlerController extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Method not supported");
+        tokens.put("errorCode", METHOD_NOT_ALLOWED);
+        ErrorResponse error = new ErrorResponse(METHOD_NOT_ALLOWED.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Media Type not supported");
+        tokens.put("errorCode", UNSUPPORTED_MEDIA_TYPE);
+        ErrorResponse error = new ErrorResponse(UNSUPPORTED_MEDIA_TYPE.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Media Type not acceptable");
+        tokens.put("errorCode", UNSUPPORTED_MEDIA_TYPE);
+        ErrorResponse error = new ErrorResponse(UNSUPPORTED_MEDIA_TYPE.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Missing path");
+        tokens.put("errorCode", BAD_REQUEST);
+        ErrorResponse error = new ErrorResponse(BAD_REQUEST.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Bad Request failed");
+        tokens.put("errorCode", BAD_REQUEST);
+        ErrorResponse error = new ErrorResponse(BAD_REQUEST.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleServletRequestBindingException(ServletRequestBindingException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Request failed");
+        tokens.put("errorCode", BAD_REQUEST);
+        ErrorResponse error = new ErrorResponse(BAD_REQUEST.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleConversionNotSupported(ConversionNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Conversion Not Supported");
+        tokens.put("errorCode", UNSUPPORTED_MEDIA_TYPE);
+        ErrorResponse error = new ErrorResponse(UNSUPPORTED_MEDIA_TYPE.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Type Mismatch");
+        tokens.put("errorCode", BAD_REQUEST);
+        ErrorResponse error = new ErrorResponse(BAD_REQUEST.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Missing body");
+        tokens.put("errorCode", BAD_REQUEST);
+        ErrorResponse error = new ErrorResponse(BAD_REQUEST.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotWritable(HttpMessageNotWritableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Not posible to write");
+        tokens.put("errorCode", BAD_REQUEST);
+        ErrorResponse error = new ErrorResponse(BAD_REQUEST.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Argument Not valid");
+        tokens.put("errorCode", NOT_ACCEPTABLE);
+        ErrorResponse error = new ErrorResponse(NOT_ACCEPTABLE.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestPart(MissingServletRequestPartException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Request Not valid");
+        tokens.put("errorCode", BAD_REQUEST);
+        ErrorResponse error = new ErrorResponse(BAD_REQUEST.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", " server error Internal");
+        tokens.put("errorCode", BAD_GATEWAY);
+        ErrorResponse error = new ErrorResponse(BAD_GATEWAY.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Internal server error");
+        tokens.put("errorCode", BAD_GATEWAY);
+        ErrorResponse error = new ErrorResponse(BAD_GATEWAY.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleAsyncRequestTimeoutException(AsyncRequestTimeoutException ex, HttpHeaders headers, HttpStatus status, WebRequest webRequest) {
+
+        Map<String, Object> tokens = new HashMap<>();
+        tokens.put("errorMessage", "Timeout Async");
+        tokens.put("errorCode", REQUEST_TIMEOUT);
+        ErrorResponse error = new ErrorResponse(BAD_GATEWAY.getReasonPhrase(), tokens);
+
+        return this.handleExceptionInternal(ex, error, headers, status, webRequest);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
 
     @ExceptionHandler(RecordNotFoundException.class)
     public final ResponseEntity<ErrorResponse> handleUserNotFoundException
