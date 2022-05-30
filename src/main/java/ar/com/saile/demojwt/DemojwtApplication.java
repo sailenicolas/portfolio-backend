a9bf6285e490af7104f2de4fc9b74d3b667de4e7
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-05-29 22:58:16 -0300

    Not working yet

--- src/main/java/ar/com/saile/demojwt/DemojwtApplication.java
+++ src/main/java/ar/com/saile/demojwt/DemojwtApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemojwtApplication {
@@ -87,7 +91,7 @@ public class DemojwtApplication {
             userService.addRoleToUser("john", "ROLE_ADMIN");
             userService.addRoleToUser("john", "ROLE_USER");
 
-            
+
             userService.addRoleToUser("will", "ROLE_MANAGER");
             userService.addRoleToUser("jim", "ROLE_ADMIN");
             userService.addSoftSkillsToUser("john", appSoftSkill);
@@ -101,4 +105,19 @@ public class DemojwtApplication {
 
         };
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://jwt-frontend-38da0.web.app"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, accept", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
 }
