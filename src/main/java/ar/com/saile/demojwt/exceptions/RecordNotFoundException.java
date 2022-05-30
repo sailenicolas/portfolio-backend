d0aacc7a6026f7c3a452685f42d4507bed864e32
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-05-29 22:33:53 -0300

    Not working

--- src/main/java/ar/com/saile/demojwt/exceptions/RecordNotFoundException.java
+++ src/main/java/ar/com/saile/demojwt/exceptions/RecordNotFoundException.java
@@ -3,12 +3,9 @@ package ar.com.saile.demojwt.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.io.Serial;
-
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class RecordNotFoundException extends RuntimeException {
 
-    @Serial
     private static final long serialVersionUID = 1L;
 
     public RecordNotFoundException(String message) {
