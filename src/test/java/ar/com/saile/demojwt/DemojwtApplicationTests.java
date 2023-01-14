d8e549f61a7a482e3a02d83bc7f2dead37d40390
Elias Nicolas <sailenicolas@users.noreply.github.com>
2023-01-13 23:19:08 -0300

    Go go go

--- src/test/java/ar/com/saile/demojwt/DemojwtApplicationTests.java
+++ src/test/java/ar/com/saile/demojwt/DemojwtApplicationTests.java
@@ -2,8 +2,10 @@ package ar.com.saile.demojwt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = {"test"})
 class DemojwtApplicationTests {
 
     @Test
